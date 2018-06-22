336314ca4a691837a5ed26275ecf4d5c4be81239
lhuynh27 <leannehuynh27@gmail.com>
2018-06-22 13:53:14 -0700

    Implement send and fetch method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,40 @@
-// YOUR CODE HERE:
+var app = {
+  init: function() {},
+  send: function() {
+        var message = {
+            username: 'Mel Brooks',
+            text: 'It\'s good to be the king',
+            roomname: 'lobby'
+        };
+		$.ajax({
+  			url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  			type: 'POST',
+  			data: JSON.stringify(message),
+  			contentType: 'application/json',
+  			success: function (data) {
+    			console.log('chatterbox: Message sent');
+  			},
+  			error: function (data) {
+    			// See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    			console.error('chatterbox: Failed to send message', data);
+  			}
+		})
+	},
+	fetch: function() {
+		$.ajax({
+  			url: undefined,
+  			type: 'GET',
+  			data: JSON.stringify(message),
+  			contentType: 'application/json',
+  			success: function (data) {
+    			console.log('chatterbox: Message sent');
+  			},
+  			error: function (data) {
+    			// See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    			console.error('chatterbox: Failed to send message', data);
+  			}
+		})	
+	},
+	clearMessage: function() {
+	},
+};
